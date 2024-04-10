b1eb37c8d909b836b12f8c3d44f69a167175b48d
evkost <mrtkostoev@gmail.com>
2024-04-10 13:08:53 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
+import {WorkspaceComponent} from "./workspace/workspace.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'workspace/:id', component: WorkspaceComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];

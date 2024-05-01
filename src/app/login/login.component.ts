6087635a436afe02f1ad7e6076f71f69fd66af7d
evkost <mrtkostoev@gmail.com>
2024-05-02 00:25:17 +0300

    login component inited

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,72 @@
-import {Component} from '@angular/core';
-import {NgClass} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {AsyncPipe, NgClass} from "@angular/common";
+import {ComponentStore, provideComponentStore} from "@ngrx/component-store";
+import {LoginState, LoginStore} from "./login.store";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Router, RouterLink} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, FormsModule, ReactiveFormsModule, AsyncPipe, RouterLink, HttpClientModule],
+  providers: [provideComponentStore(LoginStore)],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  isError = false;
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(4)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+  isError$ = this.loginStore.isError$;
+  isLoading$ = this.loginStore.isLoading$;
 
-  onNextClick() {
-    this.isError = !this.isError;
+  constructor(private readonly loginStore: LoginStore, private router: Router) {}
+
+  ngOnInit() {
+    this.loginStore.email$.subscribe({
+      next: value => this.loginForm.patchValue({ email: value })
+    })
+    this.loginStore.password$.subscribe({
+      next: value => this.loginForm.patchValue({ password: value })
+    })
+    this.loginStore.isLoginCompleted$.subscribe({
+      next: value => value && this.navigateToHome()
+    })
+  }
+
+  onSubmit() {
+    this.loginStore.login({
+      email: this.loginForm.value.email!,
+      password: this.loginForm.value.password!
+    })
+  }
+
+  private navigateToHome() {
+    this.router.navigate(["/home"]).then();
   }
 }
+
+
+ // loginForm = new FormGroup({
+ //    email: new FormControl('', [Validators.required, Validators.minLength(4)]),
+ //    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+ //  });
+//
+//   constructor(private router: Router, private loginService: LoginService) {
+//   }
+//
+//   onLogin() {
+//     from(this.loginService.login(
+//       this.loginForm.value.email as string,
+//       this.loginForm.value.password as string,
+//     )).subscribe( {
+//       next: result => {
+//         console.log(result)
+//       },
+//       error: er => {
+//         console.error(er)
+//       }
+//     })
+//   }

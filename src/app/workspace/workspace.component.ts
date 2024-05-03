8776a05d6d955484443cf864259559d4d5613035
Artemzakar <artem.zakar@gmail.com>
2024-05-04 02:31:15 +0300

    window for add projects

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {SidebarComponent} from "./sidebar/sidebar.component";
 import {ProjectComponent} from "./project/project.component";
+import  {AddProjectDialogComponent} from "./add-project-dialog/add-project-dialog.component";
 import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
   imports: [
-    SidebarComponent, ProjectComponent, RouterOutlet
+    SidebarComponent, ProjectComponent, RouterOutlet, AddProjectDialogComponent
   ],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.css'

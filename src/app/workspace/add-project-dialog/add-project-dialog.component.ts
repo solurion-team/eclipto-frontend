98b93f578b67b52f0db4052690a2f7b524e702ff
Artemzakar <artem.zakar@gmail.com>
2024-05-04 22:28:47 +0300

    dialog

--- src/app/workspace/add-project-dialog/add-project-dialog.component.ts
+++ src/app/workspace/add-project-dialog/add-project-dialog.component.ts
@@ -65,42 +65,60 @@
 
 
 
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatDialog, MatDialogActions, MatDialogContent, MatDialogRef} from '@angular/material/dialog';
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-add-project-dialog',
   templateUrl: './add-project-dialog.component.html',
   standalone: true,
-  styleUrls: ['./add-project-dialog.component.css']
+  imports: [
+    CommonModule,
+    MatDialogContent,
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput,
+    MatButton,
+    MatDialogActions
+  ],
+  styleUrls: ['./add-project-dialog.component.css'],
+  providers: [
+    { provide: MatDialogRef, useValue: AddProjectDialogComponent } // Предоставляем пустой объект в качестве MatDialogRef
+  ]
 })
-export class AddProjectDialogComponent implements OnInit {
-  projectForm: FormGroup | undefined;
 
-  constructor(
-    private dialogRef: MatDialogRef<AddProjectDialogComponent>,
-    private formBuilder: FormBuilder
-  ) { }
+export class AddProjectDialogComponent {
+  projectForm: FormGroup;
+  showForm: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private dialog: MatDialog) {
     this.projectForm = this.formBuilder.group({
       name: ['', Validators.required],
-      description: ['', Validators.required]
+      description: ['']
     });
   }
 
-  closeModal(): void {
-    this.dialogRef.close();
+  toggleForm() {
+    this.showForm = !this.showForm;
   }
 
-  submitForm(): void {
+  closeModal() {
+    this.dialog.closeAll(); // Закрыть все открытые диалоговые окна
+  }
+
+  submitForm() {
     if (this.projectForm.valid) {
-      // Do something with the form data, like submitting it to a service
-      // For example: this.projectService.addProject(this.projectForm.value);
-      this.dialogRef.close();
+      console.log('Form submitted:', this.projectForm.value);
+      this.projectForm.reset();
+      this.showForm = false;
+      this.dialog.closeAll(); // Закрыть все открытые диалоговые окна после успешной отправки формы
     } else {
-      // Handle form validation errors
+      console.error('Form is invalid.');
     }
   }
 }

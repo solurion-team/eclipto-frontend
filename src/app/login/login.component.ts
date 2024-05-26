b69883d99f74995aeb309f092a3b006d4f9b6bcc
Artemzakar <artem.zakar@gmail.com>
2024-05-26 21:33:02 +0300

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit} from '@angular/core';
-import {AsyncPipe, NgClass} from "@angular/common";
+import {AsyncPipe, NgClass, NgIf} from "@angular/common";
 import {ComponentStore, provideComponentStore} from "@ngrx/component-store";
 import {LoginState, LoginStore} from "./login.store";
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Router, RouterLink} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {MatButtonToggle} from "@angular/material/button-toggle";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgClass, FormsModule, ReactiveFormsModule, AsyncPipe, RouterLink, HttpClientModule],
+  imports: [NgClass, FormsModule, ReactiveFormsModule, AsyncPipe, RouterLink, HttpClientModule, MatFormField, MatInput, MatButton, MatButtonToggle, NgIf, MatLabel],
   providers: [provideComponentStore(LoginStore)],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'

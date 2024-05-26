cc66d957426889e83c9bb4f2fc9be7e154897465
1EgorSergeevich1 <162616082+1EgorSergeevich1@users.noreply.github.com>
2024-05-26 16:47:57 +0300

    Update delete-confirmation-dialog.component.ts

--- src/app/home/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
+++ src/app/home/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
@@ -1,1 +1,21 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-confirmation-dialog',
+  templateUrl: './delete-confirmation-dialog.component.html',
+  styleUrls: ['./delete-confirmation-dialog.component.css'],
+  standalone: true
+})
+export class DeleteConfirmationDialogComponent {
+  constructor(public dialogRef: MatDialogRef<DeleteConfirmationDialogComponent>) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onYesClick(): void {
+    this.dialogRef.close(true);
+  }
+}
 
